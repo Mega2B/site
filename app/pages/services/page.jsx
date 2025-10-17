df068b5dbcf78e26ba1accee4b576d7b26b01e08
Hebert <hebertdev82@gmail.com>
2025-10-17 17:04:07 -0300

    feat: services page

--- app/pages/services/page.jsx
+++ app/pages/services/page.jsx
@@ -3,6 +3,7 @@ import Logo from "../../components/logo";
 import Contact from "../../components/contact";
 import Footer from "../../components/footer";
 import Credits from "../../components/credits";
+import Image from "next/image";
 
 export default function Services() {
     return (
@@ -10,18 +11,48 @@ export default function Services() {
             <Head />
             <Logo />
 
-            <div>
-                <p className="p-25 text-4xl text-center">Serviços</p>
-                
-                <p className="p-25" id="eletricpanels">Painéis Elétricos <br /> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea nulla molestias doloribus totam, perferendis eius eum repudiandae sequi animi laudantium recusandae eveniet, repellat voluptate illo velit harum? Eius, aliquid pariatur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione asperiores nisi doloribus perferendis nobis ipsam a, blanditiis eum, facilis delectus omnis, dolores quae rem! Repellat quae sed eos eius numquam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse similique libero exercitationem, veritatis quidem perspiciatis, nesciunt, illum eveniet architecto non facere voluptatibus quae quos quia molestias laborum commodi pariatur alias. Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore dolorum, maxime rerum tenetur animi, ea porro nemo fuga illo aliquid eligendi eaque nulla? Ut numquam, alias laudantium debitis quibusdam natus!</p>
+            <div className="p-12">
+                <p className="pb-16 text-4xl text-center font-bold">Serviços</p>
 
-                <p className="p-25" id="reinstalation">Reinstalação <br /> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea nulla molestias doloribus totam, perferendis eius eum repudiandae sequi animi laudantium recusandae eveniet, repellat voluptate illo velit harum? Eius, aliquid pariatur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione asperiores nisi doloribus perferendis nobis ipsam a, blanditiis eum, facilis delectus omnis, dolores quae rem! Repellat quae sed eos eius numquam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse similique libero exercitationem, veritatis quidem perspiciatis, nesciunt, illum eveniet architecto non facere voluptatibus quae quos quia molestias laborum commodi pariatur alias. Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore dolorum, maxime rerum tenetur animi, ea porro nemo fuga illo aliquid eligendi eaque nulla? Ut numquam, alias laudantium debitis quibusdam natus!</p>
+                <div className="flex">
 
-                <p className="p-25" id="inspection">Vistoria <br /> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea nulla molestias doloribus totam, perferendis eius eum repudiandae sequi animi laudantium recusandae eveniet, repellat voluptate illo velit harum? Eius, aliquid pariatur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione asperiores nisi doloribus perferendis nobis ipsam a, blanditiis eum, facilis delectus omnis, dolores quae rem! Repellat quae sed eos eius numquam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse similique libero exercitationem, veritatis quidem perspiciatis, nesciunt, illum eveniet architecto non facere voluptatibus quae quos quia molestias laborum commodi pariatur alias. Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore dolorum, maxime rerum tenetur animi, ea porro nemo fuga illo aliquid eligendi eaque nulla? Ut numquam, alias laudantium debitis quibusdam natus!</p>
+                    <div className="flex-1">
+                        <Image src="/services1.jpeg" alt="Serviços" width={600} height={400} className="rounded-lg shadow-lg mb-8" />
+                    </div>
 
-                <p className="p-25" id="eletricprojects">Projetos Elétricos <br /> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea nulla molestias doloribus totam, perferendis eius eum repudiandae sequi animi laudantium recusandae eveniet, repellat voluptate illo velit harum? Eius, aliquid pariatur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione asperiores nisi doloribus perferendis nobis ipsam a, blanditiis eum, facilis delectus omnis, dolores quae rem! Repellat quae sed eos eius numquam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse similique libero exercitationem, veritatis quidem perspiciatis, nesciunt, illum eveniet architecto non facere voluptatibus quae quos quia molestias laborum commodi pariatur alias. Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore dolorum, maxime rerum tenetur animi, ea porro nemo fuga illo aliquid eligendi eaque nulla? Ut numquam, alias laudantium debitis quibusdam natus!</p>
+                    <div className="flex-1">
+                        <p className="pb-8" id="eletricpanels">
+                            <span className="font-bold">Painéis Elétricos</span>
+                            <br />
+                            Desenvolvemos e instalamos painéis elétricos sob medida, garantindo eficiência, segurança e conformidade com as normas técnicas. Nossos projetos são pensados para otimizar o controle e a distribuição de energia em sistemas residenciais, comerciais e industriais.
+                        </p>
 
-                <p className="p-25" id="panelassembly">Montagem de Painéis <br /> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea nulla molestias doloribus totam, perferendis eius eum repudiandae sequi animi laudantium recusandae eveniet, repellat voluptate illo velit harum? Eius, aliquid pariatur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione asperiores nisi doloribus perferendis nobis ipsam a, blanditiis eum, facilis delectus omnis, dolores quae rem! Repellat quae sed eos eius numquam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse similique libero exercitationem, veritatis quidem perspiciatis, nesciunt, illum eveniet architecto non facere voluptatibus quae quos quia molestias laborum commodi pariatur alias. Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore dolorum, maxime rerum tenetur animi, ea porro nemo fuga illo aliquid eligendi eaque nulla? Ut numquam, alias laudantium debitis quibusdam natus!</p>
+                        <p className="pb-8" id="reinstalation">
+                            <span className="font-bold">Reinstalação</span>
+                            <br />
+                            Realizamos reinstalação elétrica completa ou parcial com foco em segurança, desempenho e adequação às novas demandas de energia. Nossa equipe assegura a atualização das estruturas elétricas de acordo com os padrões mais recentes.
+                        </p>
+
+                        <p className="pb-8" id="inspection">
+                            <span className="font-bold">Vistoria</span>
+                            <br />
+                            Oferecemos serviços de vistoria elétrica detalhada para identificar falhas, riscos e oportunidades de melhoria. Emitimos relatórios técnicos com laudos precisos, garantindo total segurança e conformidade com as normas vigentes.
+                        </p>
+
+                        <p className="pb-8" id="eletricprojects">
+                            <span className="font-bold">Projetos Elétricos</span>
+                            <br />
+                            Elaboramos projetos elétricos personalizados, contemplando desde o dimensionamento da carga até a viabilidade técnica e econômica. Trabalhamos com foco em eficiência energética, sustentabilidade e segurança operacional.
+                        </p>
+
+                        <p className="pb-8" id="panelassembly">
+                            <span className="font-bold">Montagem de Painéis</span>
+                            <br />
+                            Prestamos serviços completos de montagem de painéis elétricos, utilizando componentes de alta qualidade e técnicas rigorosas de montagem. Cada painel é projetado para garantir durabilidade, confiabilidade e fácil manutenção.
+                        </p>
+                    </div>
+
+                </div>
 
             </div>
 
