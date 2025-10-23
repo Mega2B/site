9926a3c78237c5ff1029c1471d25744d054d7006
Hebert <hebertdev82@gmail.com>
2025-10-23 11:20:51 -0300

    feat: config layout.js Google

--- app/layout.js
+++ app/layout.js
@@ -9,14 +9,16 @@ const rubik = Rubik({
 });
 
 export const metadata = {
-  title: "Mega 2b Engenharia",
-  description: "Mega 2b Engenharia",
+  title: "Mega2B Engenharia",
+  description: "Qualidade na prestação de serviços elétricos e instalações seguras de qualidade. Conte com a Mega2B Engenharia para soluções elétricas confiáveis e eficientes.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
-      <GoogleAnalytics />
+      <head>
+        <GoogleAnalytics />
+      </head>
       <body
         className={`${rubik.variable} antialiased`}
       >
